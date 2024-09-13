ad9f4b5e70f650e6264c3a10acabfff77349c78e
jooda00 <dragon2000a@gmail.com>
2024-09-13 15:49:32 +0900

    feat: first setting

---
+++ src/main/java/com/birdbook/birdbook/BirdbookApplication.java
@@ -0,0 +1,13 @@
+package com.birdbook.birdbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birdbook/birdbook/BirdbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birdbook.birdbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
