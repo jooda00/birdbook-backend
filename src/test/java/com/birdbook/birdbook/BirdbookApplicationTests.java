76247cc51361c0ce569d01210861973951d612a1
jooda00 <dragon2000a@gmail.com>
2024-09-14 11:33:24 +0900

    feat: main test에 classes 속성 추가 (#1)

--- src/test/java/com/birdbook/birdbook/BirdbookApplicationTests.java
+++ src/test/java/com/birdbook/birdbook/BirdbookApplicationTests.java
@@ -3,7 +3,7 @@ package com.birdbook.birdbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BirdbookApplicationTests.class)
 class BirdbookApplicationTests {
 
 	@Test
