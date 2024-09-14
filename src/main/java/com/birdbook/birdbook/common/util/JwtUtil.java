72915d5e025e3084abfd9389f5bc12b0aaf4e549
jooda00 <dragon2000a@gmail.com>
2024-09-14 18:22:42 +0900

    chore: jwt util 클래스 생성 (#3)

---
+++ src/main/java/com/birdbook/birdbook/common/util/JwtUtil.java
@@ -0,0 +1,66 @@
+package com.birdbook.birdbook.common.util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JwtUtil {
+
+	private static final Logger log = LoggerFactory.getLogger(JwtUtil.class);
+	private SecretKey secretKey;
+
+	public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+		secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+			Jwts.SIG.HS256.key().build().getAlgorithm());
+	}
+
+	public String getUsername(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("username", String.class);
+	}
+
+	public String getRole(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.get("role", String.class);
+	}
+
+	public Boolean isExpired(String token) {
+		return Jwts.parser()
+			.verifyWith(secretKey)
+			.build()
+			.parseSignedClaims(token)
+			.getPayload()
+			.getExpiration()
+			.before(new Date());
+	}
+
+	public String generateToken(String username, String role, Long expiredMs) {
+		String token = Jwts.builder()
+			.claim("username", username)
+			.claim("role", role)
+			.issuedAt(new Date(System.currentTimeMillis()))
+			.expiration(new Date(System.currentTimeMillis() + expiredMs))
+			.signWith(secretKey)
+			.compact();
+		log.info("jwt 토큰 생성 : {}", token);
+		return token;
+	}
+}
