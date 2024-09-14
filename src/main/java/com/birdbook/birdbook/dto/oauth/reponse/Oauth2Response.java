8cf8fea0da13789010ee0eafa6a1b1667a97256c
jooda00 <dragon2000a@gmail.com>
2024-09-14 14:59:12 +0900

    style: naver 코딩 컨벤션 적용 (#3)

--- src/main/java/com/birdbook/birdbook/dto/oauth/reponse/Oauth2Response.java
+++ src/main/java/com/birdbook/birdbook/dto/oauth/reponse/Oauth2Response.java
@@ -1,7 +1,9 @@
-package com.birdbook.birdbook.dto.oauth;
+package com.birdbook.birdbook.dto.oauth.reponse;
 
 public interface Oauth2Response {
 	String getProviderId();
+
 	String getProvider();
+
 	String getName();
 }
