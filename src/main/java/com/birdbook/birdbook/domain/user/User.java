d205059271a096af8866f8a634029e4b1d9b5ebb
jooda00 <dragon2000a@gmail.com>
2024-09-14 15:44:02 +0900

    feat: User 엔티티에 정적 메소드 추가 및 생성자, 빌더에 access level 지정 (#3)

--- src/main/java/com/birdbook/birdbook/domain/user/User.java
+++ src/main/java/com/birdbook/birdbook/domain/user/User.java
@@ -5,9 +5,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
 public class User {
 
 	@Id
@@ -16,4 +25,12 @@ public class User {
 	private String username;
 	private String name;
 	private String role;
+
+	public static User of(String username, String name, String role) {
+		return User.builder()
+			.username(username)
+			.name(name)
+			.role(role)
+			.build();
+	}
 }
