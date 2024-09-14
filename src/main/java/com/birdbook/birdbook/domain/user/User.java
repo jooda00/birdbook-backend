6f1ade4d22dd3bf1676fcaf8754130c4312759db
jooda00 <dragon2000a@gmail.com>
2024-09-14 15:11:47 +0900

    feat: User 엔티티 작성 (#3)

---
+++ src/main/java/com/birdbook/birdbook/domain/user/User.java
@@ -0,0 +1,19 @@
+package com.birdbook.birdbook.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String name;
+	private String role;
+}
