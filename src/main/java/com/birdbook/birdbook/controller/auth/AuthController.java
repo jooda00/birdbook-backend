63b27f98c5a304e07a4eddf5df183052ad69d34e
jooda00 <dragon2000a@gmail.com>
2024-09-14 22:52:05 +0900

    feat: 로그인 API 작성 (#3)
    - 인증 코드 -> 액세스 토큰 -> JWT 순서로 생성

---
+++ src/main/java/com/birdbook/birdbook/controller/auth/AuthController.java
@@ -0,0 +1,36 @@
+package com.birdbook.birdbook.controller.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.birdbook.birdbook.common.util.JwtUtil;
+import com.birdbook.birdbook.dto.kakao.response.KakaoUserRes;
+import com.birdbook.birdbook.service.kakao.KakaoService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final KakaoService kakaoService;
+	private final JwtUtil jwtUtil;
+	private final UserDetailsService userDetailsService;
+
+	@PostMapping("/login/oauth2/code/kakao")
+	public ResponseEntity<String> login(@RequestParam String code) {
+		// 1. 인증 코드로 액세스 토큰 요청
+		String accessToken = kakaoService.getAccessToken(code);
+
+		// 2. 액세스 토큰으로 사용자 정보 요청
+		KakaoUserRes userInfo = kakaoService.getUserInfo(accessToken);
+
+		// 3. 사용자 정보로 JWT 생성
+		String token = jwtUtil.generateToken(userInfo.getUsername(), userInfo.getRole(), 60 * 60 * 1000L);
+
+		return ResponseEntity.ok(token);
+	}
+}
