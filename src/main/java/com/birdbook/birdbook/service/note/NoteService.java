e8c68ad74ec81511247ae9385f9081a8fd66cd29
jooda00 <dragon2000a@gmail.com>
2024-09-20 18:20:35 +0900

    feat: 노트 삭제 service 메소드 작성 (#15)
    - deleteNote()

--- src/main/java/com/birdbook/birdbook/service/note/NoteService.java
+++ src/main/java/com/birdbook/birdbook/service/note/NoteService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.birdbook.birdbook.domain.book.Book;
 import com.birdbook.birdbook.domain.note.Note;
+import com.birdbook.birdbook.dto.note.request.NoteDeleteReq;
 import com.birdbook.birdbook.dto.note.request.NoteReq;
 import com.birdbook.birdbook.repository.book.BookRepository;
 import com.birdbook.birdbook.repository.note.NoteRepository;
@@ -23,9 +24,19 @@ public class NoteService {
 	public Note saveNote(NoteReq req) {
 		Book book = bookRepository.findById(req.getBookId())
 			.orElseThrow(() -> new IllegalArgumentException("도서가 존재하지 않습니다."));
-
 		Note note = Note.of(req, book);
 		noteRepository.save(note);
 		return note;
+	}
+
+	@Transactional
+	public Long deleteNote(NoteDeleteReq req) {
+		Note note = noteRepository.findById(req.getNoteId())
+			.orElseThrow(() -> new IllegalArgumentException("작성한 노트가 존재하지 않습니다."));
+		if (note.isDeleted()) {
+			throw new IllegalArgumentException("이미 삭제된 노트입니다.");
+		}
+		note.deleteNote();
+		return note.getId();
 	}
 }
