1125d7df9264cb31ad7b683833d76e952cc0c900
jooda00 <dragon2000a@gmail.com>
2024-09-20 20:21:52 +0900

    feat: 생성일 순 내림차순 쿼리 메소드 생성 (#19)

--- src/main/java/com/birdbook/birdbook/repository/note/NoteRepository.java
+++ src/main/java/com/birdbook/birdbook/repository/note/NoteRepository.java
@@ -1,8 +1,11 @@
 package com.birdbook.birdbook.repository.note;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.birdbook.birdbook.domain.note.Note;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+	List<Note> findAllByOrderByCreatedAtDesc();
 }
